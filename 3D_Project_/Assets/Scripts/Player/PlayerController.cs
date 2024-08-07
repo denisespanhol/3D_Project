9be6a3dd0a8017bb05a545d05341582348c61395
Denis Espanhol <denisgfespanhol@gmail.com>
2024-08-07 13:39:18 -0300

    Add State Machine

--- 3D_Project_/Assets/Scripts/Player/PlayerController.cs
+++ 3D_Project_/Assets/Scripts/Player/PlayerController.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public enum PlayerStates
+    {
+        IDLE,
+        RUNNING,
+        JUMPING
+    }
+
+    public StateMachine<PlayerStates> playerStateMachine;
+
     [SerializeField] private float _speed = 5f;
     [SerializeField] private float _jumpForce = 5f;
     [SerializeField] private LayerMask _groundLayer;
@@ -13,22 +22,49 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
+        playerStateMachine = new StateMachine<PlayerStates>();
+        playerStateMachine.Init();
+        playerStateMachine.RegisterStates(PlayerStates.IDLE, new StateBase());
+        playerStateMachine.RegisterStates(PlayerStates.RUNNING, new StateBase());
+        playerStateMachine.RegisterStates(PlayerStates.JUMPING, new StateBase());
+
+        playerStateMachine.SwitchState(PlayerStates.IDLE);
         _rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
+        HandleMovement();
+        HandleJump();        
+    }
+
+    private void HandleMovement()
+    {
         float moveHorizontal = Input.GetAxis("Horizontal") * _speed;
         float moveVertical = Input.GetAxis("Vertical") * _speed;
 
+        if (moveHorizontal == 0 && moveVertical == 0)
+        {
+            playerStateMachine.SwitchState(PlayerStates.IDLE);
+        }
+        else
+        {
+            playerStateMachine.SwitchState(PlayerStates.RUNNING);
+        }
+        
+
         Vector3 movement = new(moveHorizontal, _rb.velocity.y, moveVertical);
         _rb.velocity = movement;
+    }
 
-        _isGrounded = Physics.CheckSphere(transform.position, 0.1f, _groundLayer);
+    private void HandleJump()
+    {
+        _isGrounded = Physics.CheckSphere(transform.position, 0.5f, _groundLayer);
 
         if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
         {
             _rb.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+            playerStateMachine.SwitchState(PlayerStates.JUMPING);
         }
     }
 }
