4da95b1bdaf1971bbf24372a2883e44decd4606c
Denis Espanhol <denisgfespanhol@gmail.com>
2024-08-12 10:17:13 -0300

    Updating project assets

---
+++ EBAC_Game3D/Assets/Scripts/Singleton.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    public static T instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = GetComponent<T>();
+        else
+            Destroy(instance);
+    }
+}
