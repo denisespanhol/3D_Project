8c77499ee251509815109cbfa639b5bf20046a5e
Denis Espanhol <denisgfespanhol@gmail.com>
2024-09-05 14:11:04 -0300

    Creating Enemy Base Script

---
+++ EBAC_Game3D/Assets/Scripts/Enemies/EnemyBase.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBase : MonoBehaviour
+{
+
+    public float startLife = 10f;
+
+    [SerializeField] private float _currentLife;
+
+    private void Awake()
+    {
+        Init();
+    }
+
+    protected void ResetLife()
+    {
+        _currentLife = startLife;
+    }
+
+    protected virtual void Init()
+    {
+        ResetLife();
+    }
+
+    protected virtual void Kill()
+    {
+        OnKill();
+    }
+
+    protected virtual void OnKill()
+    {
+        Destroy(gameObject);
+    }
+
+    public void OnDamage(float f)
+    {
+        _currentLife -= f;
+
+        if (_currentLife <= 0)
+        {
+            Kill();
+        }
+    }
+}
